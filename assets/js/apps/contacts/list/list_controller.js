1c76e27cd39d38434ab616c9fd878a6afd521482
hanishi <h.nishi@mosa-architect.com>
2014-06-20 00:37:21 +0900

    updated

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -5,6 +5,9 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
             var contactsListView = new List.Contacts({
                 collection: contacs
             });
+            contactsListView.on("itemview:contact:show", function(childView, model){
+               ContactManager.ContactsApp.Show.Controller.showContact(model);
+            });
             contactsListView.on("itemview:contact:delete", function(childView, model){
                 contacs.remove(model);
             });
