deff6bd92d40df34acd8424151be35e482c6f5fd
hanishi <h.nishi@mosa-architect.com>
2014-06-22 23:16:46 +0900

    updated

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -6,10 +6,11 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
                 collection: contacs
             });
             contactsListView.on("itemview:contact:show", function(childView, model){
-               ContactManager.ContactsApp.Show.Controller.showContact(model);
+               ContactManager.trigger("contact:show", model.get("id"));
+
             });
             contactsListView.on("itemview:contact:delete", function(childView, model){
-                contacs.remove(model);
+                model.destroy();
             });
             ContactManager.mainRegion.show(contactsListView);
         }
--- assets/js/apps/contacts/list/show_controller.js
+++ assets/js/apps/contacts/list/show_controller.js
@@ -1,9 +1,17 @@
 ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbone, Marionette, $, _) {
     Show.Controller = {
-        showContact: function(model) {
-            var contactView = new Show.Contact({
-                model: model
-            });
+        showContact: function(id) {
+            var contact = ContactManager.request("contact:entities", id);
+            var contactView;
+
+            if (contact !== undefined) {
+                var contactView = new Show.Contact({
+                    model: contact
+                });
+            }
+            else {
+                contactView = new Show.MissingContact();
+            }
 
             ContactManager.mainRegion.show(contactView);
         }
